543890329c637e5529263a0aa14c4f0f1bea8762
Danial Rahimy <daniel_rahimi@outlook.com>
2020-09-04 23:46:36 +0430

    temp

--- public/index.php
+++ public/index.php
@@ -4,31 +4,53 @@ use Monolog\Formatter\LineFormatter;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
-$startTime = microtime(true);
-
-require_once "../app/bootstrap.php";
-
-$logger = new Logger('usage');
-
 try {
+    $startTime = microtime(true);
+
+    require_once "../app/bootstrap.php";
+
     $dispatcher = new Dispatcher($routes);
 }catch (ExceptionUser $error){
+
     echo $error->getMessage();
 }catch (ExceptionDev $error){
+
     echo $error->getMessage();
 } finally {
-    $endTime = microtime(true);
-    $time = round($endTime - $startTime, 1);
-    $ram = round(memory_get_usage() / 1024 / 1024, 1);
-    $log = $_SERVER["REQUEST_URI"] . " => time: $time ms / ram: $ram mb";
-    $stream = new StreamHandler(ROOT . "/logs/usage.log", Logger::INFO);
-    $logger->pushHandler($stream);
-    $dateFormat = "Y/n/j,G:i";
 
-    $output = "%datetime% > %level_name% > %message% \n";
+    try{
+        $dateFormat = "Y/n/j,G:i";
+        $output = "%datetime% > %level_name% > %message% \n";
 
-    $formatter = new LineFormatter($output, $dateFormat);
-    $stream->setFormatter($formatter);
-    $logger->info($log);
+        $formatter = new LineFormatter($output, $dateFormat);
+
+        if (
+            strpos($_SERVER["REQUEST_URI"], "/public/") !== false or
+            strpos($_SERVER["REQUEST_URI"], "/files/") !== false
+        ){
+
+            $log = $_SERVER["REQUEST_URI"];
+
+            $logger = new Logger('not_found');
+            $stream = new StreamHandler(ROOT . "/logs/not_found_files.log", Logger::WARNING);
+            $logger->pushHandler($stream);
+            $stream->setFormatter($formatter);
+            $logger->warning($log);
+        }else{
+
+            $endTime = microtime(true);
+            $time = round($endTime - $startTime, 1);
+            $ram = round(memory_get_usage() / 1024 / 1024, 1);
+            $log = $_SERVER["REQUEST_URI"] . " => time: $time ms / ram: $ram mb";
+
+            $logger = new Logger('usage');
+            $stream = new StreamHandler(ROOT . "/logs/usage.log", Logger::INFO);
+            $logger->pushHandler($stream);
+            $stream->setFormatter($formatter);
+            $logger->info($log);
+        }
+    }catch (Exception $e){
+
+    }
 }
 
